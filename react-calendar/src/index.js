e9ed74a97b1b5f56cf89301a92ccde3ac43c03cb
adriano-551 <32731609+adriano-551@users.noreply.github.com>
2021-09-21 18:02:05 +0100

    Added bootstrap Default initializing of calendar to current date

--- react-calendar/src/index.js
+++ react-calendar/src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Calendar from './Calendar';
 import reportWebVitals from './reportWebVitals';
 
+require('bootstrap');
+
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+  integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+  crossorigin="anonymous"
+/>
+
+const currentDate = new Date(Date.now());
+{ /*Debug logging*/ console.log(currentDate) }
+
 ReactDOM.render(
   <React.StrictMode>
-    <Calendar />
+	<Calendar 
+	  currentDate={currentDate}
+	/>
   </React.StrictMode>,
   document.getElementById('root')
 );
