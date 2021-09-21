0ccb1d33ba16c5d827d8dc56b69710aee3bbdf6a
adriano-551 <32731609+adriano-551@users.noreply.github.com>
2021-09-21 18:02:45 +0100

    Placeholder month component

---
+++ react-calendar/src/Month.js
@@ -0,0 +1,14 @@
+function Month(props) {
+    return(
+        <div className="row Month">
+            { /*for each day in month, render day box */}
+            <div className="row">row 1</div>
+            <div className="row">row 2</div>
+            <div className="row">row 3</div>
+            <div className="row">row 4</div>
+            <div className="row">row 5</div>
+        </div>
+    );
+}
+
+export default Month;
