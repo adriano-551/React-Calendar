dff3a8c8f58337ae0a52f5947ba9e8f394df65d0
adriano-551 <32731609+adriano-551@users.noreply.github.com>
2021-09-23 19:33:21 +0100

    Incorporated header into calendar js added button functionality to change month and year

--- react-calendar/src/Calendar.js
+++ react-calendar/src/Calendar.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import './Calendar.css';
-import Header from './Header';
 import Month from './Month';
 
 class Calendar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {currentDate: new Date()};
-  }
 
-  render() {
-    return (
-      <div className="container">
-        <Header
-          currentDate={this.state.currentDate}
-        />
-        <Month
-          currentDate={this.state.currentDate}
-        />
-      </div>
-    );
+    this.changeDate = this.changeDate.bind(this);
   }
 
   changeDate(newDate) {
@@ -27,16 +15,43 @@ class Calendar extends React.Component {
       currentDate: newDate
     });
   }
+
+  render() {
+    const previousMonth = new Date(this.state.currentDate.valueOf() - 2629800000);
+    const nextMonth = new Date(this.state.currentDate.valueOf() + 2629800000);
+      
+    const previousYear = new Date(this.state.currentDate.valueOf() - 31557600000);
+    const nextYear = new Date(this.state.currentDate.valueOf() + 31557600000);
+
+    return (
+      <div className="container">
+        <header className="row my-4">
+          <div className="col col-lg-6">
+            <div className="row">
+              <div className="col-4 btn-group" role="group" aria-label="Month selection group">
+                  <button type="button" className="btn btn-dark col-2 text-end" onClick={() => this.changeDate(previousMonth)}>&lt; { /* Button to change to previous month*/ }</button>
+                  <button type="button" className="btn btn-dark col-8 text-center">{new Intl.DateTimeFormat('en-UK', {month:'long'}).format(this.state.currentDate)}</button>
+                  <button type="button" className="btn btn-dark col-2 text-start" onClick={() => this.changeDate(nextMonth)}>&gt; { /* Button to change to next month*/ }</button>
+              </div>
+              <div className="col-3 btn-group" role="group" aria-label="Year selection group">
+                  <button type="button" className="btn btn-dark col-1 text-end" onClick={() => this.changeDate(previousYear)}>&lt; { /* Button to change to previous year*/ }</button>
+                  <button type="button" className="btn btn-dark col-5 text-center">{this.state.currentDate.getFullYear()}</button>
+                  <button type="button" className="btn btn-dark col-1 text-start" onClick={() => this.changeDate(nextYear)}>&gt; { /* Button to change to next year*/ }</button>
+              </div>
+            </div>
+          </div>
+      </header>
+
+      <Month
+        currentDate={this.state.currentDate}
+      />
+      </div>
+    );
+  }
 }
 
 function addReminder(reminder) {
   //Adding reminder
-}
-
-
-function changeTime(newTime) {
-  // Function to change month
-  //<Calendar currentDate={currentDate} />
 }
 
 export default Calendar;
